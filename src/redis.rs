008d6de869c49d50089628b249f47f5bbb2d279e
Pure White <wudi.daniel@bytedance.com>
2024-08-09 13:18:23 +0800

    feat: bump redis to 0.26 (#13)

--- src/redis.rs
+++ src/redis.rs
@@ -10,19 +10,25 @@ impl redis::ToRedisArgs for crate::FastStr {
 impl redis::FromRedisValue for crate::FastStr {
     fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
         match v {
-            #[cfg(feature = "redis-unsafe")]
-            redis::Value::Data(bytes) => {
-                Ok(unsafe { Self::new(std::str::from_utf8_unchecked(bytes)) })
-            }
-            #[cfg(not(feature = "redis-unsafe"))]
-            redis::Value::Data(bytes) => Ok(Self::new(std::str::from_utf8(bytes)?)),
             redis::Value::Nil => Ok(Self::empty()),
             redis::Value::Int(v) => Ok(Self::new(itoa::Buffer::new().format(*v))),
-            redis::Value::Status(s) => Ok(Self::new(s)),
             redis::Value::Okay => Ok(Self::from_static_str("OK")),
-            _ => Err(redis::RedisError::from((
+            #[cfg(feature = "redis-unsafe")]
+            redis::Value::BulkString(v) => {
+                Ok(unsafe { Self::new(std::str::from_utf8_unchecked(v)) })
+            }
+            #[cfg(not(feature = "redis-unsafe"))]
+            redis::Value::BulkString(v) => Ok(Self::new(std::str::from_utf8(v)?)),
+            redis::Value::SimpleString(v) => Ok(Self::new(v)),
+            redis::Value::Double(v) => Ok(Self::new(ryu::Buffer::new().format(*v))),
+            redis::Value::Boolean(v) => {
+                Ok(Self::from_static_str(if *v { "true" } else { "false" }))
+            }
+            redis::Value::BigNumber(v) => Ok(Self::from_string(v.to_string())),
+            e => Err(redis::RedisError::from((
                 redis::ErrorKind::TypeError,
                 "Invalid response type",
+                format!("{:?}", e),
             ))),
         }
     }
@@ -30,17 +36,21 @@ impl redis::FromRedisValue for crate::FastStr {
     fn from_owned_redis_value(v: redis::Value) -> redis::RedisResult<Self> {
         match v {
             #[cfg(feature = "redis-unsafe")]
-            redis::Value::Data(bytes) => Ok(unsafe { Self::from_vec_u8_unchecked(bytes) }),
+            redis::Value::BulkString(v) => Ok(unsafe { Self::from_vec_u8_unchecked(v) }),
             #[cfg(not(feature = "redis-unsafe"))]
-            redis::Value::Data(bytes) => Self::from_vec_u8(bytes)
+            redis::Value::BulkString(v) => Self::from_vec_u8(v)
                 .map_err(|_| (redis::ErrorKind::TypeError, "Invalid UTF8").into()),
             redis::Value::Nil => Ok(Self::empty()),
             redis::Value::Int(v) => Ok(Self::new(itoa::Buffer::new().format(v))),
-            redis::Value::Status(s) => Ok(Self::from_string(s)),
             redis::Value::Okay => Ok(Self::from_static_str("OK")),
-            _ => Err(redis::RedisError::from((
+            redis::Value::SimpleString(v) => Ok(Self::from_string(v)),
+            redis::Value::Double(v) => Ok(Self::new(ryu::Buffer::new().format(v))),
+            redis::Value::Boolean(v) => Ok(Self::from_static_str(if v { "true" } else { "false" })),
+            redis::Value::BigNumber(v) => Ok(Self::from_string(v.to_string())),
+            e => Err(redis::RedisError::from((
                 redis::ErrorKind::TypeError,
                 "Invalid response type",
+                format!("{:?}", e),
             ))),
         }
     }
