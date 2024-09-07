8c80ebd3a89b769086534be891426dbf56626238
CPunisher <1343316114@qq.com>
2024-09-07 12:31:04 +0800

    Update enum variant name

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ mod size_asserts {
     static_assertions::assert_eq_size!(super::FastStr, [u8; 40]); // 40 bytes
 }
 
-const INLINE_CAP: usize = 32;
+const INLINE_CAP: usize = InlineSize32::_V32 as usize;
 
 impl FastStr {
     /// Create a new `FastStr` from any type `T` that can be converted to a string slice
@@ -625,7 +625,7 @@ impl From<Cow<'static, str>> for FastStr {
 #[derive(Clone, Copy)]
 #[repr(usize)]
 enum InlineSize1 {
-    InlineSize1 = 1,
+    _V1 = 1,
 }
 
 impl InlineSize1 {
@@ -638,7 +638,7 @@ impl InlineSize1 {
 #[derive(Clone, Copy)]
 #[repr(usize)]
 enum InlineSize2 {
-    InlineSize2 = 2,
+    _V2 = 2,
 }
 
 impl InlineSize2 {
@@ -651,8 +651,8 @@ impl InlineSize2 {
 #[derive(Clone, Copy)]
 #[repr(usize)]
 enum InlineSize4 {
-    InlineSize3 = 3,
-    InlineSize4,
+    _V3 = 3,
+    _V4,
 }
 
 impl InlineSize4 {
@@ -665,10 +665,10 @@ impl InlineSize4 {
 #[derive(Clone, Copy)]
 #[repr(usize)]
 enum InlineSize8 {
-    InlineSize5 = 5,
-    InlineSize6,
-    InlineSize7,
-    InlineSize8,
+    _V5 = 5,
+    _V6,
+    _V7,
+    _V8,
 }
 
 impl InlineSize8 {
@@ -681,14 +681,14 @@ impl InlineSize8 {
 #[derive(Clone, Copy)]
 #[repr(usize)]
 enum InlineSize16 {
-    InlineSize9 = 9,
-    InlineSize10,
-    InlineSize11,
-    InlineSize12,
-    InlineSize13,
-    InlineSize14,
-    InlineSize15,
-    InlineSize16,
+    _V9 = 9,
+    _V10,
+    _V11,
+    _V12,
+    _V13,
+    _V14,
+    _V15,
+    _V16,
 }
 
 impl InlineSize16 {
@@ -701,22 +701,22 @@ impl InlineSize16 {
 #[derive(Clone, Copy)]
 #[repr(usize)]
 enum InlineSize32 {
-    InlineSize17 = 17,
-    InlineSize18,
-    InlineSize19,
-    InlineSize20,
-    InlineSize21,
-    InlineSize22,
-    InlineSize23,
-    InlineSize24,
-    InlineSize25,
-    InlineSize26,
-    InlineSize27,
-    InlineSize28,
-    InlineSize29,
-    InlineSize30,
-    InlineSize31,
-    InlineSize32,
+    _V17 = 17,
+    _V18,
+    _V19,
+    _V20,
+    _V21,
+    _V22,
+    _V23,
+    _V24,
+    _V25,
+    _V26,
+    _V27,
+    _V28,
+    _V29,
+    _V30,
+    _V31,
+    _V32,
 }
 
 impl InlineSize32 {
